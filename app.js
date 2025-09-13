905eeb2b268d65b0eeed87c9a0702b229a940984
MounirHouliche <mounir_rojos@outlook.es>
2025-09-13 18:57:56 +0100

    added ReadMe files

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ buttonNext.textContent = '▶';
 const images = [
     {src:  'https://images.pexels.com/photos/212324/pexels-photo-212324.jpeg', alt: 'Grass field'},
     {src:  'https://images.pexels.com/photos/257092/pexels-photo-257092.jpeg', alt: 'Mountain in Rishikesh, India' },
-    {src:  'https://www.pexels.com/es-es/foto/cuerpo-de-agua-bajo-cielos-azules-y-blancos-1533720/', alt: 'Palm trees in Fuvahmulah, Maldives'},
+    {src:  'https://images.pexels.com/photos/1024975/pexels-photo-1024975.jpeg', alt: 'Palm trees in Fuvahmulah, Maldives'},
 ];
 
 let currentIndex = 0;
@@ -18,23 +18,28 @@ let currentIndex = 0;
 function createThumbnails() {
     thumbnails.innerHTML = '';
     images.forEach((img, i) => {
-        const thumbnailImg = document.createElement('img');
-        thumbnailImg.src = img.src;
-        thumbnailImg.alt = img.alt;
-        thumbnailImg.addEventListener('click', () => showImage(i));
-        thumbnails.appendChild(thumbnailImg);
+        const t = document.createElement('img');
+        t.src = img.src;
+        t.alt = img.alt;       
+        t.addEventListener('click', () => showImage(i));
+        thumbnails.appendChild(t);
     });
 }
 
 function showImage(i) {
     if (i < 0 || i >= images.length) return;
     currentIndex = i;
-    displayContainer = '';
+    displayContainer.innerHTML = '';
     const big = document.createElement('img');
     big.src = images [i].src;
     big.alt = images [i].alt;
     displayContainer.appendChild(big);
 }
+
+
+buttonPrev.addEventListener('click', () => showImage(currentIndex - 1));
+buttonNext.addEventListener('click', () => showImage(currentIndex + 1));
+
 
 createThumbnails();
 showImage(0);
